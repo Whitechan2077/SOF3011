cfcbd67a6dcca9ea6fce599523f55b9014e7757c
Hoàng Dũng <dungbui8198@gmail.com>
2023-08-07 23:17:22 +0700

    nha

---
+++ Silde1/src/main/java/servlet/Servlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet", value = "/Servlet")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("test","Vai long");
+        RequestDispatcher rd = request.getRequestDispatcher("Hello.jsp");
+        rd.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
