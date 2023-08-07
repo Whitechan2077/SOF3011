9e3dca7b4ec6d8b8f5b7e0282fc4c6d3ff67cc1a
Hoàng Dũng <dungbui8198@gmail.com>
2023-08-07 22:42:01 +0700

    nah

---
+++ ServletDemo1/src/main/java/com.poly.controller.web/HomeController.java
@@ -0,0 +1,25 @@
+package com.poly.controller.web;
+
+import com.poly.model.UserModel;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/home")
+public class HomeController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("/view/home/Home.jsp");
+        rd.forward(req,resp);
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ ServletDemo1/src/main/java/com/poly/controller/web/AdminController.java
@@ -0,0 +1,21 @@
+package com.poly.controller.web;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "AdminController", urlPatterns= "/admin-page")
+public class AdminController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            RequestDispatcher rd = request.getRequestDispatcher("/view/admin/admin.jsp");
+            rd.forward(request ,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
